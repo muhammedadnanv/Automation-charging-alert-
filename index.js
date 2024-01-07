0228259a9125c2c22c4f8ec35f0952bda883447c
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-07 10:10:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const say = require('say');
 const isPhoneCharging = () => {
   try {
     // Use a platform-specific command to check the charging status
-    const chargingStatus = execSync('node index.js', { encoding: 'utf-8' });
+    const chargingStatus = execSync('Say', { encoding: 'utf-8' });
     return chargingStatus.trim() === 'charging';
   } catch (error) {
     console.error('Error checking charging status:', error.message);
